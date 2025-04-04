a846e6e88d47b58c279d3aeb3b3a29dc2e7a31bc
Erish Prajapati <irishmjn@gmail.com>
2025-04-04 08:08:07 +0545

    OpenWeatherAPI Fetched

---
+++ backend/utils.py
@@ -0,0 +1,12 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+
+def get_weather(lat,lon):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}& units = metric"
+    response = requests.url(url)
+    return response.json()
+
